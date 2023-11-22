8df93f50d6ccf14c9313fff899c1efdd7792c4c3
tusker4 <tusker4@naver.com>
2023-11-22 22:25:04 +0900

    feat : 파일구조 ~ 초기컨테이너

--- romme/src/App.js
+++ romme/src/App.js
@@ -1,24 +1,17 @@
+/* eslint-disable */
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, Link } from 'react-router-dom'
+import Admin from './pages/admin/Admin';
 
-function App() {
+
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='WebContainer'>
+      <Routes>
+        <Route path="/admin" element={<Admin />} />
+      </Routes>
     </div>
   );
 }
---
+++ romme/src/pages/admin/Admin.js
@@ -0,0 +1,18 @@
+/* eslint-disable */
+import React, { useState } from 'react'; // useState를 가져오기
+
+
+function Admin() {
+    return(
+        <div>
+            테스트!
+            <div className={`TestBox`}>
+                테스트2!
+            </div>
+
+        </div>
+    )
+
+}
+
+export default Admin;
